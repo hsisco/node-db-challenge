617bcb37d9e55acdd55a9d3dbccb650e5d3d1ad0
hsisco <hysensisco@gmail.com>
2020-04-19 21:43:09 -0400

    Update 20200419212919_migrations.js

--- data/migrations/20200419212919_migrations.js
+++ data/migrations/20200419212919_migrations.js
@@ -1,13 +1,29 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable("generic_table", table => {
-    table.increments();
-    table.integer("integercolumn").notNullable().unique();
-    table.text("textcolumn");
-    table.boolean("booleancolumn");
-  })
+  return knex.schema
+    .createTable("projects", table => {
+      table.increments();
+      table.varchar("name", 128).notNullable();
+      table.varchar("description", 400);
+      table.boolean("completed").defaultTo(false);
+    })
+    .createTable("resources", table => {
+      table.increments();
+      table.varchar("name", 128).notNullable().unique();
+      table.varchar("description", 400);
+    })
+    .createTable("tasks", table => {
+      table.increments();
+      table.integer('project_id').unsigned().references('projects.id')
+      table.varchar("description", 400).notNullable();
+      table.varchar("notes", 400);
+      table.boolean("completed").defaultTo(false);
+    })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("generic_table")
+  return knex.schema
+    .dropTableIfExists("tasks")
+    .dropTableIfExists("resources")
+    .dropTableIfExists("projects")
 };
